e4e780640b925b60aa69f74cd0d6f28d4ed9fc4d
Saju Paul <saju.christel@blu-pa.com>
2020-09-08 12:31:03 +0530

    displaying meal items

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 
 const CategoryMealsScreen = (props) => {
 
     const categoryId = props.navigation.getParam('categoryId');
-    const selectedCategory = CATEGORIES.find(cat => cat.id === categoryId);
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0);
+
+    const renderMealItem = itemData => {
+        return (
+            <View>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        )
+    }
 
     return (
         <View style={styles.screen}>
             <Text>The category meals screen:</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="See mail details" onPress={() => {
-                props.navigation.push('MealDetail')
-            }} />
-
-            <Button title="Go back" onPress={() => {
-                props.navigation.goBack()
-            }} />
+            <FlatList data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem} />
         </View>
     )
 }
