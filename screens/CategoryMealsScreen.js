f036a5c654ebbc64ba00e76dc13920732b1934b9
Saju Paul <saju.christel@blu-pa.com>
2020-09-08 13:04:53 +0530

    styling meal items

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 import { CATEGORIES, MEALS } from '../data/dummy-data';
-
+import MealItem from '../components/MealItem'
 
 const CategoryMealsScreen = (props) => {
 
     const categoryId = props.navigation.getParam('categoryId');
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0);
 
-    const renderMealItem = itemData => {
-        return (
-            <View>
-                <Text>{itemData.item.title}</Text>
-            </View>
-        )
-    }
+    const renderMealItem = itemData => <MealItem
+        onSelectMeal={() => { }}
+        image={itemData.item.imageUrl}
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability} />
 
     return (
         <View style={styles.screen}>
-            <Text>The category meals screen:</Text>
             <FlatList data={displayedMeals}
                 keyExtractor={(item, index) => item.id}
-                renderItem={renderMealItem} />
+                renderItem={renderMealItem}
+                style={{ width: '100%' }} />
+
         </View>
     )
 }
