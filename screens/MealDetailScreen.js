496f1f787a2b399b6c0bb5dd28374293e7d5a2b0
Saju Paul <saju.christel@blu-pa.com>
2020-09-09 15:12:38 +0530

    Adding meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,19 +1,39 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, ScrollView, Image } from 'react-native'
 import { MEALS } from '../data/dummy-data';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../components/HeaderButton'
+import DefaultText from '../components/DefaultText'
 
+
+const ListItem = props => {
+    return (
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    )
+}
 const MealDetailScreen = (props) => {
     const mealId = props.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return (
-        <View style={styles.screen}>
-            <Text>The meal details screen: meal detail{JSON.stringify(selectedMeal, 4)}</Text>
-            <Button title="Got back to categories" onPress={() => {
-                props.navigation.popToTop();
-            }} />
-        </View>
+        <ScrollView>
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration} m</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient =>
+                <ListItem key={ingredient}>{ingredient}</ListItem>)}
+
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map(step =>
+                <ListItem key={step}>{step}</ListItem>)}
+
+        </ScrollView>
+
     )
 }
 
@@ -32,12 +52,27 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 
 }
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    image: {
+        width: '100%',
+        height: 200
     },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: 'green',
+        borderLeftWidth: 2,
+        padding: 10
+    }
 })
 
 export default MealDetailScreen
