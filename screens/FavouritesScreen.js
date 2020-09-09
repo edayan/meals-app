232ab582361c9750559c45f5c3ef6a26e1554b39
Saju Paul <saju.christel@blu-pa.com>
2020-09-09 21:55:51 +0530

    Adding default text

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
-
+import DefaultText from '../components/DefaultText'
 
 
 const FavouritesScreen = (props) => {
 
     const favoriteMeals = useSelector(state => state.meals.favoriteMeals)
+
+    if (favoriteMeals.length === 0 || !favoriteMeals) {
+        return (<View style={styles.content}>
+            <DefaultText>You have no favorite meals!</DefaultText>
+        </View>)
+    }
 
     return (
         <MealList listData={favoriteMeals} navigation={props.navigation} />
@@ -29,5 +36,12 @@ FavouritesScreen.navigationOptions = navData => {
 };
 
 
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default FavouritesScreen
